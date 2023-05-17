5a3519bea663b6f99e4c27fe04665ea7a9c8a2ab
Maks Biski <maksde25@gmail.com>
2023-05-17 19:12:08 +0200

    Implemented main function. Displaying image works.

--- main.c
+++ main.c
@@ -3,17 +3,90 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "swirl.h"
-#include "bmp.h"
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+
+void displayResult(RGBTRIPLE* pixelArray, int width, int height) {
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("Display Result", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return;
+    }
+
+    // Create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return;
+    }
+
+    // Create a texture
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_BGR24, SDL_TEXTUREACCESS_STATIC, width, height);
+    if (texture == NULL) {
+        printf("Texture could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return;
+    }
+
+    // Create a buffer for flipped rows
+    RGBTRIPLE* flippedBuffer = (RGBTRIPLE*)calloc(height * width, sizeof(RGBTRIPLE));
+    for (int i = 0; i < height; i++) {
+        memcpy(&flippedBuffer[i * width], &pixelArray[(height - i - 1) * width], width * sizeof(RGBTRIPLE));
+    }
+
+    // Update the texture with pixel data
+    SDL_UpdateTexture(texture, NULL, flippedBuffer, width * sizeof(RGBTRIPLE));
+
+    // Clear the renderer
+    SDL_RenderClear(renderer);
+
+    // Copy the texture to the renderer
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+    // Display the contents of the renderer
+    SDL_RenderPresent(renderer);
+
+    // Wait for the window to be closed
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    // Cleanup
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    free(flippedBuffer);
+    SDL_Quit();
+}
 
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <imagePath> <swirlFactor>\n", argv[0]);
+void initDefaultSwirlFactor(double* swirlFactor)
+{
+    *swirlFactor = 0.05;
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <imagePath>\n", argv[0]);
         return 1;
     }
 
     char* inputFile = argv[1];
-    double swirlFactor = atof(argv[2]);
 
     // Open input file
     FILE *inptr = fopen(inputFile, "r");
@@ -36,17 +109,28 @@ int main(int argc, char *argv[]) {
     int height = abs(bitmapInfoheader.biHeight);
     int width = bitmapInfoheader.biWidth;
 
-    void* pixelArray = malloc(height*width*3);
+    RGBTRIPLE* pixelArray = (RGBTRIPLE*)calloc(height * width, sizeof(RGBTRIPLE));
 
-    // Iterate over infile's scanlines
+    // Determine padding for scanlines
+    int padding = (4 - (width * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // Iterate over infile's scanlines in reverse order
     for (int i = 0; i < height; i++)
     {
+        // Calculate the offset in the pixel array for the current row
+        RGBTRIPLE* row = pixelArray + i * width;
+
         // Read row into pixel array
-        fread(pixelArray[i], sizeof(RGBTRIPLE), width, inptr);
+        fread(row, sizeof(RGBTRIPLE), width, inptr);
 
         // Skip over padding
         fseek(inptr, padding, SEEK_CUR);
     }
+
+    double swirlFactor;
+    initDefaultSwirlFactor(&swirlFactor);
+
+    displayResult(pixelArray, width, height);
 
     return 0;
 }
