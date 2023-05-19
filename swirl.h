b8068d8dd4bee9a9c3e13fe899462146e5db9f13
Maks Biski <maksde25@gmail.com>
2023-05-19 13:45:05 +0200

    Modified argument names in performSwirl declaration

--- swirl.h
+++ swirl.h
@@ -2,6 +2,6 @@
 #define _SWIRL_H
 #include <stdint.h>
 #include "bmp.h"
-void performSwirl(RGBTRIPLE *pPixelArray, int width, int height,
+void performSwirl(RGBTRIPLE *pixelArraySource, RGBTRIPLE *pixelArrayCopy, int width, int height,
                   double swirlFactor);
 #endif
